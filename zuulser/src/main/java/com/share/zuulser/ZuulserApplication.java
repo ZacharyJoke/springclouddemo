0f5d4347e13c46cf41d6e850f45bf1070ca9c56d
lxl <liuxuleiemail@163.com>
2018-09-26 23:15:09 +0800

    移除oauth2暂时停更

--- zuulser/src/main/java/com/share/zuulser/ZuulserApplication.java
+++ zuulser/src/main/java/com/share/zuulser/ZuulserApplication.java
@@ -2,7 +2,7 @@ package com.share.zuulser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//@EnableZuulProxy
+@EnableZuulProxy//开启zuul路由
 //@EnableOAuth2Sso
 public class ZuulserApplication {
 
--- zuulser/src/main/java/com/share/zuulser/config/SecurityConfig.java
+++ zuulser/src/main/java/com/share/zuulser/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.share.zuulser.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@Configuration
 //@EnableOAuth2Sso
